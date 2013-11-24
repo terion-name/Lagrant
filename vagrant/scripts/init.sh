5ba9db88a2479cd3443be4a1f5c718764d98c509
Terion <mail@terion.name>
2013-11-24 12:38:52 +0200

    add dotdeb for fpm

--- vagrant/scripts/init.sh
+++ vagrant/scripts/init.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 echo "--- Updating packages list ---"
+
+# deb http://packages.dotdeb.org wheezy all
+# deb-src http://packages.dotdeb.org wheezy all
+
+deb http://packages.dotdeb.org wheezy-php55 all
+deb-src http://packages.dotdeb.org wheezy-php55 all
+
+wget http://www.dotdeb.org/dotdeb.gpg
+sudo apt-key add dotdeb.gpg
 sudo apt-get update
 
 echo "--- Installing base packages ---"
