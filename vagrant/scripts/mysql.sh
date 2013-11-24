02e562fcec91789ba79b9955420bda874aba0e31
Terion <mail@terion.name>
2013-11-24 13:02:37 +0200

    fix

--- vagrant/scripts/mysql.sh
+++ vagrant/scripts/mysql.sh
@@ -4,7 +4,7 @@ echo "--- Instaling MySQL ---"
 
 MYSQL_PASSWORD=$1
 
-sudo apt-get install -y mysql-server mysql-client php5-mysql
-
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password ${MYSQL_PASSWORD}"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${MYSQL_PASSWORD}"
+
+sudo apt-get install -y mysql-server mysql-client php5-mysql
