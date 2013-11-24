25cbe0d980a9b326a26f87df15ded568af0c954a
Terion <mail@terion.name>
2013-11-24 23:08:15 +0200

    composer updater fix

--- vagrant/scripts/laravel_dev_packages.sh
+++ vagrant/scripts/laravel_dev_packages.sh
@@ -3,6 +3,5 @@
 PROJECT_PATH=$1
 
 echo "--- Add Laravel dev packcages ---"
-composer require way/generators:dev-master --dev
-composer require loic-sharma/profiler:1.1.* --dev
-composer require fzaninotto/faker:dev-master --dev
+cd ${PROJECT_PATH}
+composer require --dev way/generators:dev-master loic-sharma/profiler:1.1.* fzaninotto/faker:dev-master
