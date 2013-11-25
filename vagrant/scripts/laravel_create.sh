37a22114e7209d38095ca8bc77a9291ee41f80b9
Terion <mail@terion.name>
2013-11-25 22:42:15 +0200

    local env app config

--- vagrant/scripts/laravel_create.sh
+++ vagrant/scripts/laravel_create.sh
@@ -12,7 +12,6 @@ cp -r laravel-master/* ./
 rm -r -f laravel-master
 composer install
 php artisan key:generate
-mkdir ${PROJECT_PATH}/app/config/${ENV_NAME}
 
-# TODO: add local env
-# TODO: update db config if db was created
+mkdir ${PROJECT_PATH}/app/config/${ENV_NAME}
+cp ${PROJECT_PATH}/vagrant/templates/laravel_config_app.php ${PROJECT_PATH}/app/config/${ENV_NAME}/app.php
