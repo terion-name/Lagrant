7a92be6214f94ab30bfde2b8f4ec24b8a8dad372
Terion <mail@terion.name>
2013-11-25 11:20:28 +0200

    this approach should be faster

--- vagrant/scripts/laravel_create.sh
+++ vagrant/scripts/laravel_create.sh
@@ -4,10 +4,14 @@ PROJECT_PATH=$1
 
 echo "--- Create Laravel project ---"
 
-mkdir ${PROJECT_PATH}/_tmp
-
-composer create-project laravel/laravel ${PROJECT_PATH}/_tmp
+# mkdir ${PROJECT_PATH}/_tmp
+# composer create-project laravel/laravel ${PROJECT_PATH}/_tmp
+# cd ${PROJECT_PATH}
+# cp -r _tmp/* ${PROJECT_PATH}
+# rm -r -f _tmp
 
 cd ${PROJECT_PATH}
-cp -r _tmp/* ${PROJECT_PATH}
-rm -r -f _tmp
+wget https://github.com/laravel/laravel/archive/master.zip
+unzip -o master.zip && rm master.zip
+composer install
+php artisan key:generate
