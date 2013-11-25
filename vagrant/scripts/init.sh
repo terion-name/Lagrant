91cc4e6e8943a1cad488c9f5002810ebac2b191e
Terion <mail@terion.name>
2013-11-25 11:38:46 +0200

    zip unzip

--- vagrant/scripts/init.sh
+++ vagrant/scripts/init.sh
@@ -5,4 +5,4 @@ echo "--- Updating packages list ---"
 sudo apt-get update
 
 echo "--- Installing base packages ---"
-sudo apt-get install -y vim curl git-core build-essential openssl libssl-dev python-software-properties python g++ make
+sudo apt-get install -y vim curl git-core build-essential openssl libssl-dev python-software-properties python g++ make zip unzip
