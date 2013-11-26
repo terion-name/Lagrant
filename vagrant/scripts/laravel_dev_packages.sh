8849571a1f356f3bc8d78cd6771b36a8db65acf2
Terion <mail@terion.name>
2013-11-26 14:44:16 +0200

    added dev packages and updated installer

--- vagrant/scripts/laravel_dev_packages.sh
+++ vagrant/scripts/laravel_dev_packages.sh
@@ -1,9 +1,36 @@
 #!/usr/bin/env bash
 
 PROJECT_PATH=$1
+ENV_NAME=$2
 
 echo "--- Add Laravel dev packcages ---"
-cd ${PROJECT_PATH}
-composer require --dev way/generators:dev-master loic-sharma/profiler:1.1.* fzaninotto/faker:dev-master
 
-# TODO: update config
+# update composer.json
+
+cd ${PROJECT_PATH}
+composer require --dev --no-update loic-sharma/profiler:1.1.* way/generators:dev-master way/laravel-test-helpers:dev-master barryvdh/laravel-ide-helper:1.*
+composer require --dev --no-update fzaninotto/faker:dev-master codeception/codeception:* phpunit/phpunit=3.7.*
+composer update
+
+# update configs
+
+cd ${PROJECT_PATH}/app/config/${ENV_NAME}
+
+PROVIDERS="'Way\Generators\GeneratorsServiceProvider', 'Profiler\ProfilerServiceProvider', 'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider'"
+FACADES="'Profiler' => 'Profiler\Facades\Profiler'"
+
+sed -i "s/{{PROVIDERS_ARRAY}}/$PROVIDERS/" ./app.php
+sed -i "s/{{ALIASES_ARRAY}}/$FACADES/" ./app.php
+
+# publish packages configs
+
+cd ${PROJECT_PATH}
+
+php artisan config:publish loic-sharma/profiler --env="${ENV_NAME}"
+php artisan config:publish barryvdh/laravel-ide-helper --env="${ENV_NAME}"
+
+# generate IDE helper
+
+php artisan clear-compiled --env="${ENV_NAME}"
+php artisan ide-helper:generate --env="${ENV_NAME}"
+php artisan optimize --env="${ENV_NAME}"
