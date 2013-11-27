e39b5c32b2fe9051e874810def4c2da217009aae
Terion <mail@terion.name>
2013-11-27 20:58:46 +0200

    change dev packages registration method
    https://github.com/laravel/framework/issues/1603#issuecomment-21864164

--- vagrant/scripts/laravel_dev_packages.sh
+++ vagrant/scripts/laravel_dev_packages.sh
@@ -8,19 +8,27 @@ echo "--- Add Laravel dev packcages ---"
 # update composer.json
 
 cd ${PROJECT_PATH}
+composer require rtablada/package-installer
 composer require --dev --no-update loic-sharma/profiler:1.1.* way/generators:dev-master way/laravel-test-helpers:dev-master barryvdh/laravel-ide-helper:1.*
 composer require --dev --no-update fzaninotto/faker:dev-master codeception/codeception:* phpunit/phpunit=3.7.*
 composer update
 
 # update configs
 
-cd ${PROJECT_PATH}/app/config/${ENV_NAME}
+cd ${PROJECT_PATH}/app/start
 
-PROVIDERS="'Way\\\Generators\\\GeneratorsServiceProvider', 'Profiler\\\ProfilerServiceProvider', 'Barryvdh\\\LaravelIdeHelper\\\IdeHelperServiceProvider'"
-FACADES="'Profiler' => 'Profiler\\\Facades\\\Profiler'"
+# create a start file for development environment
+echo '<?php\n\n' > ${ENV_NAME}.php
 
-sed -i "s/{{PROVIDERS_ARRAY}}/$PROVIDERS/" ./app.php
-sed -i "s/{{ALIASES_ARRAY}}/$FACADES/" ./app.php
+# register providers via start file (see https://github.com/laravel/framework/issues/1603#issuecomment-21864164)
+echo 'App::register('Way\\Generators\\GeneratorsServiceProvider');' > ${ENV_NAME}.php
+echo 'App::register('Profiler\\ProfilerServiceProvider');' > ${ENV_NAME}.php
+echo 'App::register('Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider');' > ${ENV_NAME}.php
+
+echo '\n\n' > ${ENV_NAME}.php
+
+# register aliases
+echo 'App::bind('Profiler' => 'Profiler\\Facades\\Profiler');' > ${ENV_NAME}.php
 
 # publish packages configs
 
@@ -34,3 +42,10 @@ php artisan config:publish barryvdh/laravel-ide-helper --env="${ENV_NAME}"
 php artisan clear-compiled --env="${ENV_NAME}"
 php artisan ide-helper:generate --env="${ENV_NAME}"
 php artisan optimize --env="${ENV_NAME}"
+
+
+#cd ${PROJECT_PATH}/app/config/${ENV_NAME}
+#PROVIDERS="'Way\\\Generators\\\GeneratorsServiceProvider', 'Profiler\\\ProfilerServiceProvider', 'Barryvdh\\\LaravelIdeHelper\\\IdeHelperServiceProvider'"
+#FACADES="'Profiler' => 'Profiler\\\Facades\\\Profiler'"
+#sed -i "s/{{PROVIDERS_ARRAY}}/$PROVIDERS/" ./app.php
+#sed -i "s/{{ALIASES_ARRAY}}/$FACADES/" ./app.php
