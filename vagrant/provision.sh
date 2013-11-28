2e2427dfc4ebbbb11b1e3c7b95f36b5b886c9f37
Terion <mail@terion.name>
2013-11-28 13:00:52 +0200

    refactor

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -63,7 +63,7 @@ then
     then
         ${SCRIPTS}/laravel_set_db.sh $PROJECT_PATH $ENV_NAME $DATABASE_TYPE $DATABASE_NAME $DATABASE_ROOT_PASSWORD
     fi
-    ${SCRIPTS}/laravel_dev_packages.sh $PROJECT_PATH $ENV_NAME
+    ${SCRIPTS}/laravel_packages.sh $PROJECT_PATH $ENV_NAME
 else
     ${SCRIPTS}/laravel_migrate.sh $PROJECT_PATH $ENV_NAME
 fi
