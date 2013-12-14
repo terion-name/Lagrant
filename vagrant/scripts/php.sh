0c3da1251881342678765b86ddc14929d24e5adc
Terion <mail@terion.name>
2013-12-14 20:30:06 +0200

    comment out disabled functions for CLI to enable Boris (L4.1 tinker)

--- vagrant/scripts/php.sh
+++ vagrant/scripts/php.sh
@@ -4,3 +4,6 @@ echo "--- Installing PHP and packages ---"
 sudo add-apt-repository ppa:ondrej/php5
 sudo apt-get update
 sudo apt-get install -y php5-dev php5-cli php-pear php5-curl php5-gd php5-mcrypt php5-readline
+
+# comment out disabled functions for CLI to enable Boris (L4.1 tinker)
+sed -i "s/disable_functions/;disable_functions/" /etc/php5/cli/php.ini
