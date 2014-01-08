07141ce7c1a07362c3881054387f4e136655cf6f
Terion <mail@terion.name>
2014-01-08 17:18:53 +0200

    add gmagick to cli config

--- vagrant/scripts/gmagick.sh
+++ vagrant/scripts/gmagick.sh
@@ -6,3 +6,4 @@ echo "--- Installing gmagick ---"
 sudo apt-get install -y graphicsmagick libgraphicsmagick1-dev
 sudo pecl install gmagick-1.1.4RC1
 sudo sh -c 'echo "extension=gmagick.so" >> /etc/php5/fpm/php.ini'
+sudo sh -c 'echo "extension=gmagick.so" >> /etc/php5/cli/php.ini'
