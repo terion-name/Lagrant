18ceecbf0af9fc5bf4acaa0c9b4ecce63e54e7ce
Terion <mail@terion.name>
2014-01-14 21:52:30 +0200

    set xdebug.scream to 0 to prevent larval break because of disabling @-suppressor
    see http://stackoverflow.com/a/21122536/1114816

--- vagrant/scripts/xdebug.sh
+++ vagrant/scripts/xdebug.sh
@@ -2,3 +2,7 @@
 
 echo "--- Installing and configuring Xdebug ---"
 sudo apt-get install -y php5-xdebug
+
+# this install method sets xdebug.scream to 1
+# and this breaks laravel
+sudo sed -i "s/xdebug.scream=1/xdebug.scream=0/" /etc/php5/mods-available/xdebug.ini
