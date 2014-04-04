3fb75e7cfa5fdcf5a47342b71fba1ef0451019f6
Terion <mail@terion.name>
2014-04-04 11:21:13 +0300

    ensure libsasl2 & confirm enterprise auth support

--- vagrant/scripts/mongodb.sh
+++ vagrant/scripts/mongodb.sh
@@ -10,6 +10,7 @@ sudo apt-get install mongodb-10gen
 sudo /etc/init.d/mongodb start
 
 echo "--- MongoDB PHP extension ---"
-sudo pecl install mongo
+sudo apt-get install libsasl2-dev
+sudo pecl install mongo -y
 sudo sh -c 'echo "extension=mongo.so" >> /etc/php5/fpm/php.ini'
 sudo sh -c 'echo "extension=mongo.so" >> /etc/php5/cli/php.ini'
