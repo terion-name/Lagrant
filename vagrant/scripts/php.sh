d87a0a031528853940776f5b42b7164d674337d9
Terion <mail@terion.name>
2014-04-29 13:32:16 +0300

    add SPL Types
    http://www.php.net/manual/en/intro.spl-types.php

--- vagrant/scripts/php.sh
+++ vagrant/scripts/php.sh
@@ -7,3 +7,9 @@ sudo apt-get install -y php5-dev php5-cli php-pear php5-curl php5-gd php5-mcrypt
 
 # comment out disabled functions for CLI to enable Boris (L4.1 tinker)
 sed -i "s/disable_functions/;disable_functions/" /etc/php5/cli/php.ini
+
+############
+# extensions
+############
+# SPL Types http://www.php.net/manual/en/intro.spl-types.php
+sudo pecl install SPL_Types
