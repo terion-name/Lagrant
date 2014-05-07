2ba651b7958a4038afd5140e9cc10dc1cfa19d20
Terion <mail@terion.name>
2014-05-07 14:41:32 +0300

    fixed ruby install

--- vagrant/scripts/ruby.sh
+++ vagrant/scripts/ruby.sh
@@ -2,14 +2,14 @@
 
 echo "--- Installing Ruby ---"
 
-sudo \curl -sSL https://get.rvm.io | bash -s stable
+\curl -sSL https://get.rvm.io | bash -s stable
 
 source /usr/local/rvm/scripts/rvm
 
 source /etc/profile.d/rvm.sh
 
-sudo rvm requirements
-sudo rvm install ruby
+rvm requirements
+rvm install ruby
 rvm use ruby --default
 rvm rubygems current
-sudo gem install -f rake
+gem install -f rake
