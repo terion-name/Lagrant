8c4948ead48717a076230090fe44ac411d7caded
Terion <mail@terion.name>
2014-05-07 12:26:01 +0300

    force yes

--- vagrant/scripts/mongodb.sh
+++ vagrant/scripts/mongodb.sh
@@ -6,7 +6,7 @@ echo "--- Installing MongoDB ---"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
-sudo apt-get install mongodb-10gen
+sudo apt-get install -y mongodb-10gen
 sudo /etc/init.d/mongodb start
 
 echo "--- MongoDB PHP extension ---"
