29f18b60d467b225a9d26c27cd7650e1d1a4e999
Terion <mail@terion.name>
2014-05-07 12:22:18 +0300

    envoy now installs via composer

--- vagrant/scripts/laravel_create.sh
+++ vagrant/scripts/laravel_create.sh
@@ -14,6 +14,4 @@ rm -r -f laravel-master
 mkdir ${PROJECT_PATH}/app/config/${ENV_NAME}
 cp ${PROJECT_PATH}/vagrant/templates/laravel_config_app.stub ${PROJECT_PATH}/app/config/${ENV_NAME}/app.php
 
-wget https://github.com/laravel/envoy/raw/master/envoy.phar
-sudo mv envoy.phar /usr/local/bin/envoy
-chmod +x /usr/local/bin/envoy
+composer global require "laravel/envoy=~1.0"
